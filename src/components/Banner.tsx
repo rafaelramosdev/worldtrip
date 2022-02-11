867b63ab76596e86af1d0852a6f097b9c56339f6
Rafael Ramos <rafaelramosdev@gmail.com>
2022-02-11 17:00:55 -0300

    feat: create banner component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,49 @@
+import { Box, Flex, Stack, Image, Text } from "@chakra-ui/react";
+
+export function Banner() {
+  return (
+    <Box
+      h={335}
+      bgImage="url('/images/background.png')"
+      bgPosition="center"
+    >
+      <Flex
+        maxW="1440px"
+        h="100%"
+        m="0 auto"
+        px="140px"
+        align="center"
+        justify="space-between"
+      >
+        <Stack
+          spacing="20px"
+        >
+          <Text
+            fontWeight="500"
+            fontSize="36px"
+            lineHeight="54px"
+            color="white.800"
+          >
+            5 Continentes, <br/> infinitas possibilidades
+          </Text>
+
+          <Text
+            fontWeight="normal"
+            fontSize="20px"
+            lineHeight="30px"
+            color="gray.200"
+          >
+            Chegou a hora de tirar do papel a viagem que você <br/> sempre sonhou. 
+          </Text>
+        </Stack>
+
+        <Image
+          mb={-120}
+          boxSize="400px"
+          src="/airplane.svg" 
+          alt="Airplane" 
+        />
+      </Flex>
+    </Box>
+  )
+}
