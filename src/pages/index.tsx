3defae357a9922c97e062121c09973ef19b1a406
Rafael Ramos <rafaelramosdev@gmail.com>
2022-02-11 17:00:18 -0300

    feat: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,42 @@
+import { Box, Text } from "@chakra-ui/react";
+
+import { Header } from "../components/Header";
+import { Banner } from "../components/Banner";
+import { TravelTypes } from "../components/TravelTypes";
+import { Divider } from "../components/Divider";
+import { ContinentSlide } from "../components/ContinentSlide";
+
 export default function Home() {
   return (
-    <h1>hello</h1>
+    <Box>
+      <Header />
+      <Banner />
+
+      <Box
+        as="main"
+        w="100%"
+        maxW="1440px"
+        m="0 auto"
+        pt="114px"
+        pb="40px"
+        flex="1"
+      >
+        <TravelTypes />
+
+        <Divider />
+
+        <Text
+          fontWeight="500"
+          fontSize="36px"
+          lineHeight="54px"
+          textAlign="center"
+          color="gray.700"
+        >
+          Vamos nessa? <br/> Então escolha seu continente
+        </Text>
+
+        <ContinentSlide />
+      </Box>
+    </Box>
   )
 }
