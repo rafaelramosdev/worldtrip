12e0bf9c549c2d23eaaaf7a4ac4f584346cb8bb5
Rafael Ramos <rafaelramosdev@gmail.com>
2022-02-11 13:46:15 -0300

    feat: configure app theme

---
+++ src/styles/theme.ts
@@ -0,0 +1,33 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    black: {
+      "900": "#000000",
+    },
+    gray: {
+      "700": "#47585B",
+      "500": "#999999",
+      "200": "#DADADA",
+    },
+    white: {
+      "900": "#ffffff",
+      "800": "#F5F8FA"
+    },
+    yellow: {
+      "900": "#FFBA08",
+    }
+  },
+  fonts: {
+    heading: 'Poppins',
+    body: 'Poppins',
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.900',
+        color: 'gray.50'
+      }
+    }
+  }
+})
