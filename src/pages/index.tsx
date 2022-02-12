c1e6e30d2b2b362e0955c2eb665528008139f284
Rafael Ramos <rafaelramosdev@gmail.com>
2022-02-12 13:16:29 -0300

    feat: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head';
+
 import { Box, Text } from "@chakra-ui/react";
 
 import { Banner } from "../components/Banner";
@@ -7,34 +9,40 @@ import { ContinentSlide } from "../components/ContinentSlide";
 
 export default function Home() {
   return (
-    <Box>
-      <Banner />
+    <>
+      <Head>
+        <title>Home | worldtrip</title>
+      </Head>
+  
+      <Box>
+        <Banner />
 
-      <Box
-        as="main"
-        w="100%"
-        maxW="1440px"
-        m="0 auto"
-        pt="114px"
-        pb="40px"
-        flex="1"
-      >
-        <TravelTypes />
-
-        <Divider />
-
-        <Text
-          fontWeight="500"
-          fontSize="36px"
-          lineHeight="54px"
-          textAlign="center"
-          color="gray.700"
+        <Box
+          as="main"
+          w="100%"
+          maxW="1440px"
+          m="0 auto"
+          pt="114px"
+          pb="40px"
+          flex="1"
         >
-          Vamos nessa? <br/> Então escolha seu continente
-        </Text>
-        
-        <ContinentSlide />
+          <TravelTypes />
+
+          <Divider />
+
+          <Text
+            fontWeight="500"
+            fontSize="36px"
+            lineHeight="54px"
+            textAlign="center"
+            color="gray.700"
+          >
+            Vamos nessa? <br/> Então escolha seu continente
+          </Text>
+          
+          <ContinentSlide />
+        </Box>
       </Box>
-    </Box>
+    </>
   )
 }
