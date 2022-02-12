37e5e68798dcdaaabbf93cbb24639a62b6a35a8d
Rafael Ramos <rafaelramosdev@gmail.com>
2022-02-12 11:53:51 -0300

    feat: create home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import { Box, Text } from "@chakra-ui/react";
 
-import { Header } from "../components/Header";
 import { Banner } from "../components/Banner";
 import { TravelTypes } from "../components/TravelTypes";
 import { Divider } from "../components/Divider";
