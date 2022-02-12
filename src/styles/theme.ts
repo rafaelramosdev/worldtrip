dec515d7df111ea9ec217f5f60cb14a943ed5e84
Rafael Ramos <rafaelramosdev@gmail.com>
2022-02-12 11:55:07 -0300

    feat: customize the stylization of the swiper slide

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -8,6 +8,7 @@ export const theme = extendTheme({
     gray: {
       "700": "#47585B",
       "500": "#999999",
+      '450' : 'rgba(153, 153, 153, 0.5)',
       "200": "#DADADA",
     },
     white: {
@@ -16,6 +17,7 @@ export const theme = extendTheme({
     },
     yellow: {
       "900": "#FFBA08",
+      '850' : 'rgba(255, 186, 8, 0.5)'
     }
   },
   fonts: {
@@ -27,7 +29,42 @@ export const theme = extendTheme({
       body: {
         bg: 'white.800',
         color: 'gray.700'
-      }
+      },
+      '.swiper-button-next': {
+        w: '60px',
+        h: '60px',
+        right: '20px',
+        
+        '&::after': {
+          fontSize: '3xl',
+        },
+      },
+      '.swiper-button-prev': {
+        w: '60px',
+        h: '60px',
+        left: '20px',
+
+        '&::after': {
+          fontSize: '3xl',
+        },
+      },
+      '.swiper-button-prev, .swiper-button-next': {
+        color: 'yellow.900',
+      },
+      '.swiper-button-prev.swiper-button-disabled, .swiper-button-next.swiper-button-disabled': {
+        opacity: 0,
+      },
+      '.swiper-pagination .swiper-pagination-bullet': {
+        bg: 'gray.500',
+        opacity: 1,
+        w: '16px',
+        h: '16px',
+  
+        '&.swiper-pagination-bullet-active': {
+          bg: '#ffba08',
+          opacity: 1,
+        }
+      },
     }
   }
 })
