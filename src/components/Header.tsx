1da08467710f254ce4f7fb69dfb979be88d0a9c2
Rafael Ramos <rafaelramosdev@gmail.com>
2022-02-12 13:59:38 -0300

    feat: implement functionality to show the back button in Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,36 @@
-import { Flex } from '@chakra-ui/react'
+import { useRouter } from 'next/router';
 
-export function Header() {
+import { Button, Flex, Icon } from '@chakra-ui/react'
+
+import { FiChevronLeft } from 'react-icons/fi'
+
+interface HeaderProps {
+  showBackButton?: boolean;
+}
+
+export function Header({ showBackButton = false }: HeaderProps) {
+  const { back } = useRouter();
+
   return (
     <Flex
       as="header"
+      position="relative"
       w="100%"
       h="100px"
       align="center"
       justify="center"
     >
+      { !!showBackButton && (
+        <Button
+          variant="unstyled"
+          position="absolute"
+          left="140px"
+          onClick={back}
+      >
+        <Icon as={FiChevronLeft} fontSize="32px" />
+      </Button>
+      )}
+      
       <img src="/logo.svg" alt="worldtrip" />
     </Flex>
   )
